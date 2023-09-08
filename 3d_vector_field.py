403ee312e3eb208b862374e1722d84028a6bda5a
haraldtv <harald.tverdal@protonmail.com>
2023-09-08 12:54:25 +0200

    Update 3d_vector_field.py

--- 3d_vector_field.py
+++ 3d_vector_field.py
@@ -9,18 +9,22 @@ Created on Fri Sep  8 12:44:55 2023
 import matplotlib.pyplot as plt
 import numpy as np
 
+N = 4
+XMIN = -1
+XMAX = 1
+
 ax = plt.figure().add_subplot(projection='3d')
 
 # Make the grid
-x, y, z = np.meshgrid(np.linspace(-1, 1, 8),
-                      np.linspace(-1, 1, 8),
-                      np.linspace(-1, 1, 8))
+x, y, z = np.meshgrid(np.linspace(XMIN, XMAX, N),
+                      np.linspace(XMIN, XMAX, N),
+                      np.linspace(XMIN, XMAX, N))
 
 # Make the direction data for the arrows
-u = x**2
-v = y+z
-w = z**3
+u = -y
+v = x
+w = 0
 
-ax.quiver(x, y, z, u, v, w, length=0.2, normalize=False)
+ax.quiver(x, y, z, u, v, w, length=0.6, normalize=False)
 
 plt.show()
